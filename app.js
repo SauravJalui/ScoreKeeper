3e66a3872df28dea88b24f489245980ac7b395d1
SauravJalui <saurav.jalui-codes@outlook.com>
2021-07-25 20:02:34 +0530

    added pingpong scorekeeper

---
+++ app.js
@@ -0,0 +1,55 @@
+const p1 = {
+  score: 0,
+  display: document.querySelector("#p1Display"),
+  button: document.querySelector("#p1Button"),
+};
+const p2 = {
+  score: 0,
+  display: document.querySelector("#p2Display"),
+  button: document.querySelector("#p2Button"),
+};
+
+const reset = document.querySelector("#reset");
+const playTo = document.querySelector("#playTo");
+
+let winningScore = 3;
+let isGameOver = false;
+
+function updateScores(player, opponent) {
+  if (!isGameOver) {
+    player.score++;
+    if (player.score === winningScore) {
+      isGameOver = true;
+      player.display.classList.add("has-text-success");
+      opponent.display.classList.add("has-text-danger");
+      player.button.disabled = true;
+      opponent.button.disabled = true;
+    }
+  }
+  player.display.textContent = player.score;
+}
+
+p1.button.addEventListener("click", function () {
+  updateScores(p1, p2);
+});
+
+p2.button.addEventListener("click", function () {
+  updateScores(p2, p1);
+});
+
+playTo.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  resetAll();
+});
+
+reset.addEventListener("click", resetAll);
+
+function resetAll() {
+  isGameOver = false;
+  for (let p of [p1, p2]) {
+    p.score = 0;
+    p.display.textContent = 0;
+    p.display.classList.remove("has-text-success", "has-text-danger");
+    p.button.disabled = false;
+  }
+}
